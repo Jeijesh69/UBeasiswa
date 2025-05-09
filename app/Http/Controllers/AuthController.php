73723de1e114469aaf1433c25f8cdf5b1000085b
Amos Juang <149787864+AmosJuang@users.noreply.github.com>
2025-05-09 13:09:44 +0700

    Add files via upload
    Auth Controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+class AuthController extends Controller
+{
+    public function register (Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        $user = User ::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+        ], 201);
+    }
+    public function login (Request $request)
+    {
+        $request -> validate ([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json([
+                'message' => 'Login successful',
+            ]);
+        }
+        return response()->json([
+            'message' => 'Invalid credentials',
+        ], 401);
+    }
+}

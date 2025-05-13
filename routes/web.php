e5a49b9fe19c1057d8d01db6492a8d26feeef59e
Amos Juang <149787864+AmosJuang@users.noreply.github.com>
2025-05-13 17:35:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [BlogController::class, 'home']); // 👈 changed from 'index' to 'home'
 Route::get('/home', [BlogController::class, 'home']);
 Route::get('/program', [BlogController::class, 'program']);
 Route::get('/mentor', [BlogController::class, 'mentor']);
 Route::get('/beasiswa', [BlogController::class, 'beasiswa']);
-Route::get('/login', [BlogController::class, 'login']);
-Route::get('/register', [BlogController::class, 'register']);
+Route::get('/login', function () {
+    return view('login');
+})->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', function () {
+    return view('register');
+})->name('register.form');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/forget-password', function () {
+    return view('forget-password'); 
+})->name('forget-password');
 

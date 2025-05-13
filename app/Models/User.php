2a88ed9493d99fa88c703c263a8c5169d8caa3c7
Amos Juang <149787864+AmosJuang@users.noreply.github.com>
2025-05-13 17:41:57 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,8 +33,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'id',
-        'username',
+        'name',
         'email',
         'password',
     ];
